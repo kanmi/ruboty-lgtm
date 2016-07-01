b81bbf2930939f8eee1dd8bae43ce53f39f2784c
Ryota Hanyu <m5191121@u-aizu.ac.jp>
2016-07-01 17:45:03 +0900

    Follow 302 redirection

--- lib/ruboty/lgtm/actions/lgtm.rb
+++ lib/ruboty/lgtm/actions/lgtm.rb
@@ -12,11 +12,27 @@ module Ruboty
         def get_lgtm_image_url
           res = ''
           begin
-            res = Oga.parse_html(Net::HTTP.get(URI.parse('http://www.lgtm.in/g'))).css('#imageUrl')[0].get('value')
+            res = Oga.parse_html(http_get(URI.parse('http://www.lgtm.in/g'))).css('#imageUrl')[0].get('value')
           end
 
           res
         end
+
+		def http_get(uri)
+		  5.times do |retry_count|
+			http = Net::HTTP.new(uri.host, uri.port)
+			http.use_ssl = true if uri.scheme == 'https'
+
+			res = http.get(uri.path)
+			case res
+			when Net::HTTPSuccess
+			  return res.body
+			when Net::HTTPRedirection
+			  uri = URI.parse(res['Location'])
+			  next
+			end
+		  end
+		end
       end
     end
   end
